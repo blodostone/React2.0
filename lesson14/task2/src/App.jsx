5a206484545298ce9ff67330a8615568e0a2971c
bloodstone <maks.khokholov.99@gmail.com>
2021-02-08 23:11:04 +0200

    hgfh

--- lesson14/task2/src/App.jsx
+++ lesson14/task2/src/App.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Link} from 'react-router-dom'
-import User from './User'
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import User from './User';
 
 const App = () => {
-    return (
-        <Router>
-            <div class="page">
-                <div class="page__content">
-                    <h1>Users</h1>
-                    <ul class="navigation">
-                        <li class="navigation__item">
-                            <Link to="/users/github">Github</Link>
-                        </li>
-                        <li class="navigation__item">
-                            <Link to="/users/facebook">Facebook</Link>
-                        </li>
-                    </ul>
-                    <Switch>
-                        <Route path= "/users/userId" component={User} />
-                        <Route path="/">
-                            <span>Select a user please</span>
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <div className="page">
+      <Router>
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/users/:userId">
+              <User />
+            </Route>
+            <Route path="/">
+              <span>Select a user please</span>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
